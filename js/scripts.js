a9adee7e554606d77687767c698dac07459976c5
Erica Wright and Patrick McGreevy <erica@>
2017-01-10 15:07:15 -0800

    add clickable images and jQuery function to remove them

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,36 @@ $(document).ready(function() {
   $("button#cat-button").click(function() {
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#dog-button").click(function() {
     $("ul#dog").prepend("<li>Growl!</li>");
     $("ul#cat").prepend("<li>Hiss!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("img#cat-pic").click(function() {
+    $(this).after("<img src='img/meow.jpg' alt='A picture of a cat meowing'>");
+    $(".cat-view").children("img").last().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("img#dog-pic").click(function() {
+    $(this).after("<img src='img/bark.jpg' alt='A picture of a dog barking'>");
+    $(".dog-view").children("img").last().click(function() {
+      $(this).remove();
+    });
   });
 });
